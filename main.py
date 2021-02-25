871811531b7dea1d2322ac99c1116e10881a078c
Seretis Kleanthis <serkleanthis@gmail.com>
2021-02-25 21:24:24 +0200

    Module created

--- main.py
+++ main.py
@@ -1,112 +1,56 @@
-My Drive
+main.py
+Not shared
+Type
+Text
+Size
+1 KB (1,294 bytes)
+Storage used
+4 KB (3,614 bytes)
+Location
+Python SAP Seperator
+Owner
+me
+Modified
+5:34 PM by me
+Opened
+5:33 PM by me
+Created
+Feb 21, 2021 with Google Drive Web
+Add a description
+Viewers can download
 #------------------------------------------------------------#
 # This is demo program has been developed by @kseretis for
 # business purposes. 
-# --> (input) a txt file that contains abap code
-# <-- (output) a xlsx file that contains the lines and the 
+# (input) --> a txt file that contains abap code
+# (output) <-- a xlsx file that contains the lines and the 
 # values were inside '
 #------------------------------------------------------------#
 
-from Record import Record   # Importing class from Record.py
-import xlsxwriter
+import myModule
 key = "'"                   # Global keyword
 counter = 0                 # Counter
 myList = []                 # Our List
-exceptionList = []          # Exceptions List
-preList = ["CALL FUNCTION", "VALUE", "DATA", "DEFAULT", "AUTHORITY-CHECK", "ID"]                
+exceptionList = ["CALL FUNCTION", "VALUE", "DATA", 
+                "DEFAULT", "AUTHORITY-CHECK", "ID",
+                "HAHA", "hana"]                
 
 # Opening the file
-file = open("input.txt", "r")
-
-testv = "Test"
-
-# Detect the value inside the '
-# <-- returns the values list
-def getNestedValue(raw):
-    flag = False            # Flag
-    nestedValue = ""        # Nested value
-    valuesList = []         # Nested values list
-
-    for index in raw:
-        if index == key:
-            if flag:
-                flag = False
-                valuesList.append(nestedValue)
-                nestedValue = ""
-            else:
-                flag = True
-                continue
-        if flag:
-            nestedValue += index
-
-    return valuesList
-
-# exceptions
-def fillListWithExceptions():
-    file = open("Exceptions.txt")
-    exceLines = file.read().splitlines()
-    
-    for raw in exceLines:
-        exceptionList.append(raw)
-
-    file.close()
-
-def setExceptions():
-    stopParam = True
-    print("Enter exceptions: ")
-    while stopParam:
-        tmpInput = input()
-    
-        if tmpInput == stopKeyword:
-            stopParam = False
-        exceps.append(tmpInput)
-    
-# Check the value's length, create a new record
-# & append to the list
-def createNewRecord(line, text):
-    if len(text) > 1:
-        # if text not in exceptionList:
-        newRecord = Record(line, text)
-        myList.append(newRecord)
-
-# Create new xls file and write the new data
-def writeToXls():
-    workbook = xlsxwriter.Workbook("Results.xlsx")
-    worksheet = workbook.add_worksheet()
-    index = 0
-    for obj in myList:
-        worksheet.write(index, 0, obj.line)
-        worksheet.write(index, 1, obj.value)
-        index += 1
-
-    workbook.close()
-
-def checkPreList(raw):
-    for sbString in preList:
-        if sbString in raw:
-            return False
-    return True
-
+file = open("input.txt")
 # Reading the lines from the txt file
 lines = file.readlines()
-# Reading file with exceptions
-# fillListWithExceptions()
 
 # Reading line by line the txt files
 for line in lines:
     counter += 1    # Counts the lines
-    if key in line:
-        if checkPreList(line):
-            valueList = getNestedValue(line)
+    if key in line and myModule.checkLineByLine(line, exceptionList):
+            valueList = myModule.getNestedValue(line, key)
             for index in valueList:
-                createNewRecord(counter, index)
+                myList.append(myModule.createNewRecord(counter, index))
         
-# Loop at list to print the stored elements
-for obj in myList:
-    print(obj.line , " - ", obj.value )
-
 # Call the writting func
-writeToXls()
+myModule.checkIfResultsExist(myList)
+print("File Results.xlsx have created!")
+print(len(myList) , " items have found!")
 
 # Closing the file
 file.close()
---
+++ myModule.py
@@ -0,0 +1,97 @@
+myModule.py
+Not shared
+Type
+Text
+Size
+2 KB (2,398 bytes)
+Storage used
+2 KB (2,398 bytes)
+Location
+Python SAP Seperator
+Owner
+me
+Modified
+5:32 PM by me
+Opened
+9:23 PM by me
+Created
+5:33 PM with Google Drive Web
+Add a description
+Viewers can download
+from Record import Record       # Importing class from Record.py
+import os, xlsxwriter           # libs
+
+# This function checks line by line the code
+# (input) --> line, list of exceptions
+# (output) <-- True if there is not a * or a keyword in the line
+def checkLineByLine(line, exceptionList):
+
+    # this part of code checks if the line is a comment
+    subString = line[0:3]
+    if "*" in subString:
+        return False  
+    if inExceptionsList(line, exceptionList):
+        return False
+    return True
+
+# This function scans the exceptions list
+# (input) --> line, list of exceptions
+# (output) <-- True if there is a keyword in the line
+def inExceptionsList(line, exceptionList):
+    for exceps in exceptionList:
+        if exceps in line:
+            return True
+    return False
+
+# Detect the value inside the ' ' 
+# (input) --> line, key
+# (output) <-- Returns the list filled with values
+def getNestedValue(line, key):
+    flag = False            # Flag
+    nestedValue = ""        # Nested value
+    valuesList = []         # Nested values list
+
+    for index in line:
+        if index == key:
+            if flag:
+                flag = False
+                if len(nestedValue) > 1:
+                    valuesList.append(nestedValue)
+                nestedValue = ""
+            else:
+                flag = True
+                continue
+        if flag:
+            nestedValue += index
+
+    return valuesList
+
+# Check the value's length, create a new record
+# & append it to the list
+# (input) --> line number, the value
+# (output) <-- New object
+def createNewRecord(line, value):
+    newRecord = Record(line, value)
+    return newRecord
+    
+# This function checks if the excel file with results already exists
+# if yes the script replace it with a new one
+# (input) --> the list
+def checkIfResultsExist(myList):
+    if os.path.exists("Results.xlsx"):
+        os.remove("Results.xlsx")
+        print("Old excel file deleted!")
+    writeToXlsx(myList)
+
+# Create new xls file and write the new data
+# (input) --> the list
+def writeToXlsx(myList):
+    workbook = xlsxwriter.Workbook("Results.xlsx")
+    worksheet = workbook.add_worksheet()
+    index = 0
+    for obj in myList:
+        worksheet.write(index, 0, obj.line)
+        worksheet.write(index, 1, obj.value)
+        index += 1
+
+    workbook.close()
